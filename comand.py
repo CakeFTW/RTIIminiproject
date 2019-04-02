c12db0cc54b0e24ed202c584da65abd532abd3f3
Rasmus <isager-kruuse@hotmail.com>
2019-04-02 13:35:56 +0200

    working on serial communication between ino and py

--- comand.py
+++ comand.py
@@ -10,6 +10,7 @@ import pyautogui as py
 import math
 from win32gui import GetWindowText, GetForegroundWindow
 import win32clipboard
+import serial
 
 
 
@@ -43,30 +44,74 @@ def getListOfTabs():
         i += 1
         print(a)
 
+
+    #open google chrome
     py.keyDown("alt")
     py.press("tab")
     py.keyUp("alt")
 
-    py.moveTo(screenWidth*0.5,68) #place cursor on 
-    py.click()
-    py.keyDown("ctrl")
-    py.press(["a","c"])
-    py.keyUp("ctrl")
+    py.moveTo(screenWidth*0.5,68) #place cursor on adress bar
 
 
-    win32clipboard.OpenClipboard()
-    tabs.append(win32clipboard.GetClipboardData())
 
-
-    return tabs
+    for a in range(15):
+        for _ in range(2):
+            py.click()
+            py.keyDown("ctrl")
+            py.press(["a","c","tab"])
+            py.keyUp("ctrl")
+            win32clipboard.OpenClipboard()
+            tabs.append(win32clipboard.GetClipboardData())
+            win32clipboard.CloseClipboard()
+        
+        if(tabs[:len(tabs)//2] == tabs[len(tabs)//2:]):
+            print(tabs[:len(tabs)//2])
+            print(tabs[len(tabs)//2:])
+            break
+    return tabs[:len(tabs)//2]
 
 inputTest = "https://docs.python.org/3/tutorial/datastructures.html", "https://www.youtube.com", "https://hotmail.com"
 
 
+#listening test
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = 'COM240'
+ser.open()
 
+doneSending = False
+msgToBeSent = []
 
+while 1:
+    a = ser.read_all().decode("utf-8")
+    time.sleep(3)
+    print(a)
+    time.sleep(0.05)
+    if '1' in a and not doneSending:
+        msgToBeSent.append('save')
+        msgToBeSent.append("oit mate")
+        msgToBeSent.append('done sending')
+        ser.write(bytes(str(msgToBeSent[0]).encode("ascii")))
+        doneSending = True
+    
+    
+    if "getTabs" in str(a):
+        listOfTabs = getListOfTabs()
 
-print(getListOfTabs())
+    if msgToBeSent == []:
+        continue
+
+    if msgToBeSent[0] in str(a):
+        msgToBeSent.pop(0)
+        
+    if(msgToBeSent != []):
+            continue
+
+        ser.write(bytes(str(msgToBeSent[0]).encode("ascii")))
+        print("sent the message" + str(msgToBeSent[0]))
+    else :
+        print("did not find " + str(msgToBeSent[0]) + " in " + str(a))
+
 
 
 
