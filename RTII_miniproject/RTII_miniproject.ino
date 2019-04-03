b5fab347659c13c138800440699e18dd59a25790
Rasmus <isager-kruuse@hotmail.com>
2019-04-03 15:03:24 +0200

    EEPROM READ/WRITE IMPLEMENTED

--- RTII_miniproject/RTII_miniproject.ino
+++ RTII_miniproject/RTII_miniproject.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_ESP8266.h>
+
 //________________THE TAB-SAVEr__________________
 //Real-Time Interfaces & Interaction miniproject
 //Rasmus Kruuse          Student nr.2016-3929        AAU CPH 
@@ -72,6 +74,8 @@ void loop(){
 
     if( msg == "done"){
         isReceiving = false;
+        writeString(10,memo);
+        memo = "";
     }
 
     if( msg == "sending"){
@@ -79,7 +83,9 @@ void loop(){
     }
 
     if( msg == "read"){
-        Serial.print("inMemory:" + memo + "over");
+        String stringToSend;
+        stringToSend = read_String(10);
+        Serial.print("inMemory:" + stringToSend + "over");
     }
     
 }
@@ -102,4 +108,33 @@ void performAction(unsigned long duration){ //perform action based on duration o
 
 void receiveList(){
     Serial.print("gettabsover");
+}
+
+void writeString(char add,String data)
+{
+  int _size = data.length();
+  int i;
+  for(i=0;i<_size;i++)
+  {
+    EEPROM.write(add+i,data[i]);
+  }
+  EEPROM.write(add+_size,'\0');   //Add termination null character for String Data
+}
+ 
+ 
+String read_String(char add)
+{
+  int i;
+  char data[100]; //Max 100 Bytes
+  int len=0;
+  unsigned char k;
+  k=EEPROM.read(add);
+  while(k != '\0' && len<500)   //Read until null character
+  {    
+    k=EEPROM.read(add+len);
+    data[len]=k;
+    len++;
+  }
+  data[len]='\0';
+  return String(data);
 }
